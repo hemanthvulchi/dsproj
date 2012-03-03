bb98c576402f60c9f2a7d18e6e6ab7fa7e05ff50
hemanthvulchi <hemanthvulchi@75566f1a-89ef-7d4f-ec7b-224df2291f5f>
2012-03-03 21:59:47 +0000

    commiting

---
+++ testfs.c
@@ -0,0 +1,26 @@
+/*
+*	This file has Namenode main and other functionalities
+*	This is to be executed in the namenode server
+*/
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <stdlib.h>
+#include "global.h"
+#include "ping.c"
+#include "list.c"
+#include "socket_command.c"
+#include "dd.h"
+#include "dd.c"
+
+
+// Its not already init, its the big init..
+// All of heamnath's code will go in here..
+int main()
+{
+	//Initialize all the linked lists.. Just warm up.. :)
+	int i=0;
+	printf("function started");
+	i=   dummymain();
+	printf("function ended");
+}
